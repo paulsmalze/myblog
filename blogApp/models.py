e6f361136f4b5787ae1d0a3a265e4f634e48ee97
paulsmalze <waffwegs@yahoo.com>
2021-07-03 13:55:42 -0500

    modified models and applied migrations

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class User(models.Model):
+#     firstName = models.CharField(max_length=255)
+#     lastName = models.CharField(max_length=255)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
